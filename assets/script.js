2944adc86888dee642c436e4c6ce21095da8c6e4
Berserk <alexlecerf0131@gmail.com>
2023-01-28 10:26:36 +0000

    changed format of functions in script.js file and added styling dynamically to #today id for current weather

--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,56 @@
-let headerStyle = $("header")
+var headerStyle = $("header")
     .css({ "background-image": "linear-gradient(to right, blue , darkblue)" })
 
-    var currentDate = moment();
-    
+var currentDate = moment();
 
-// let searchBtn = $("#search-button").css({"background-color":"lightblue"})
-// let searchForm = $("#search-form").css({"border-radius":"5px"})
-
-
-// API URL and API Key
-var city = "brazilia"
-var APIKey = "75f87170766a1fa219fe8b9fd08ad11b"
-var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}`
 var searchBtn = $("#search-button")
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    // console.log(queryURL)
-    console.log(response)
+searchBtn = $("#search-button").css({"background-color":"lightblue","margin-top":"10px"})
 
-    searchBtn.on("click", function (event) {
-        event.preventDefault();
+
+
+function displayWeather() {
+    // API URL and API Key
+    var inputCity = "Plymouth,UK";
+    var APIKey = "75f87170766a1fa219fe8b9fd08ad11b"
+    var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${inputCity}&appid=${APIKey}`
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        // console.log(queryURL)
+        console.log(inputCity)
+        console.log(response)
         // var currentWeather = $("#today");
-        var currentIcon = $("<img>").attr("src", "http://openweathermap.org/img/wn/"+response.list[0].weather[0].icon+"@2x.png") 
-
+        $("#today").css({"border":"solid black", "border-radius":"5px", "padding-bottom":"20px"})
+        
         $("<div>").addClass("city")
         $("<div>").addClass("temp")
         $("<div>").addClass("wind")
         $("<div>").addClass("humdity")
-
-        currentCity = response.city.name;
-        $(".city").text(response.city.name + (currentDate.format(" (DD/MM/YYYY)"))).append(currentIcon);
+        
+        var currentIcon = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + "@2x.png")
+        var currentCity = response.city.name;
+        $(".city").text(currentCity + (currentDate.format(" (DD/MM/YYYY)")))
+        .css({"font-size":"25px","font-weight":"bold"}).append(currentIcon);
         var currentTemp = Math.floor(response.list[0].main.temp - 273.15);
-        $(".temp").text(currentTemp + "C");
+        $(".temp").text("Current Temperature: " + currentTemp + " &deg;C")
+        .css({"font-size":"20px"});
         var currentWind = response.list[0].wind.speed;
-        $(".wind").text(currentWind + "Kph");
+        $(".wind").text("Wind Speed: " + currentWind + "Kph")
+        .css({"font-size":"20px"});
         var currentHumidity = response.list[0].main.humidity;
-        $(".humidity").text(currentHumidity + "%");
-
-
+        $(".humidity").text("Humidity: " + currentHumidity + "%")
+        .css({"font-size":"20px"});
+        
+        
         // currentWeather.append();
-
     })
+}
+
+searchBtn.on("click", function (event) {
+    event.preventDefault();
+    displayWeather();
+
 })
+
